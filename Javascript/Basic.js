8100a6a83b73f75f94d7689a370d346f97bf764c
rockstarrk04 <jramkumar2004@gmail.com>
2026-08-12 09:54:43 +0530

    Intro to JS Print statements

---
+++ Javascript/Basic.js
@@ -0,0 +1,25 @@
+// // write method is used to display content but without space
+// document.write("I'm in External Javascript")
+// document.write('Hello')
+// document.write('Students')
+// // output : HelloStudents
+// document.write('<br>')  // <br> is used to create new line 
+
+// writeln method is used to display content but with a space btwn two content
+document.writeln('Welcome to')
+document.writeln('Javascript class')    
+// output : Welcome to Javascript class
+
+
+// here below , document.write() allows css inside it's string and let it to be executed
+document.write('<h3 style = color:red >Welcome to JS</h3>')
+
+// console prints only in *console tab* , used only by developers
+// Inside console.log's quotes it always considers as string
+console.log("I'm in console print statement")
+console.log("<h1>console print statement</h1>")
+// output : <h1>console print statement</h1>
+
+console.warn("console warn statement")
+
+console.error("console error statement")
