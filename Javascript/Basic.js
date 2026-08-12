82afdae42cacd095d3bd7bfb810a6a2a72aae8a9
rockstarrk04 <jramkumar2004@gmail.com>
2026-08-12 16:33:20 +0530

    Adding Modified content

--- Javascript/Basic.js
+++ Javascript/Basic.js
@@ -6,12 +6,14 @@
 // document.write('<br>')  // <br> is used to create new line 
 
 // writeln method is used to display content but with a space btwn two content
-document.writeln('Welcome to')
-document.writeln('Javascript class')    
+// document.writeln('Welcome to')
+// document.writeln('Javascript class')    
 // output : Welcome to Javascript class
 
 
 // here below , document.write() allows css inside it's string and let it to be executed
+document.write("<br>")
+document.write("I'm in External Javascript")
 document.write('<h3 style = color:red >Welcome to JS</h3>')
 
 // console prints only in *console tab* , used only by developers
