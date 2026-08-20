7ae6e2c34f7f74994c93c8dd71c6931a984bb79d
rockstarrk04 <jramkumar2004@gmail.com>
2026-08-20 19:50:15 +0530

    Intro to Operators and Control Statements in Javascript

---
+++ Javascript/Control_Statements.js
@@ -0,0 +1,42 @@
+// console.error('Control Statements')
+
+
+// console.warn('If statement');
+// let num = 11
+// if (typeof(num) === 'number'){
+//     console.log('It is a number');
+// }
+
+// console.warn('If else statement');
+// let num = 11
+// if (num % 2 == 0){
+//     console.log('Even')
+// }
+// else{
+//     console.log('Odd');
+    
+// }
+
+// console.warn('Else if Ladder')
+// let num = 0
+// if (num > 0 ){
+//     console.log(num+' is +ve number')
+// }
+// else if (num < 0){
+//     console.log(num+" is -ve number"); 
+// }
+// else{
+//     console.log(num+' is neither +ve nor -ve number'); 
+// }
+
+
+console.warn('Nested If statements');
+let num = 11
+if (num > 5){
+    if (num <= 10){
+        console.log('number is valid');
+    }
+}
+else{
+    console.log('Number is invalid');
+}
---
+++ Javascript/Operators.js
@@ -0,0 +1,13 @@
+// String Concatenation
+
+let fname = 'Dinga'
+let lname = 'Raja'
+let age = 25
+
+// console.log(fname+ ' ' + lname + ' ' + 'age is ' + age + '.');
+// console.log(fname,lname,'age is',age,'.');
+
+
+console.warn(`String Interpolation`);
+
+console.log(`${fname} ${lname} age is ${age}.` );
