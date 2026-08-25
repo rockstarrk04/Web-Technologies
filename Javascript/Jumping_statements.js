ac20f57c59b2fb8013002d33e4c2f3617ff989df
rockstarrk04 <jramkumar2004@gmail.com>
2026-08-25 22:32:41 +0530

    Intro to Looping , Jumping statements in JS

---
+++ Javascript/Jumping_statements.js
@@ -0,0 +1,27 @@
+// ! jumping statemnets (break , continue , return , throw)
+console.error("Jumping statement");
+
+// todo : break
+// for(let i = 1; i <= 10 ; i++){
+//     if (i === 6)
+//         break
+//     else
+//         console.log(i);
+// }
+
+// todo : continue
+// for(let i = 1; i <= 10 ; i++){
+//     if (i === 6)
+//         continue
+//     else
+//         console.log(i);
+// }
+ 
+// todo : throw
+// let mark = Number(prompt("Enter your mark: "))
+// if (mark < 60){
+//     throw new Error("Not Eligible")
+// }
+// else{
+//     console.log("Eligible");
+// }
---
+++ Javascript/Looping_Statement.js
@@ -0,0 +1,143 @@
+// console.error('Looping Statements');
+// console.warn('for loop');
+
+//! todo : WAP to print numbers from 1-5 using for loop
+// for(let i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+
+//! todo : WAP to print even numbers from 10-20 using for loop
+// for(let i = 10; i <= 20; i = i+2){
+//     console.log(i);   
+// }
+
+//! todo : WAP to print even numbers from 30-40 in reverse order using for loop
+// for(let i = 40; i >= 30; i = i-2){
+//     console.log(i);   
+// }
+
+//! todo : WAP to print odd numbers from 10-30 using for loop
+// for(let i = 10; i <= 30; i++){
+//     if (i%2 !=0){
+//     console.log(i);   
+//     }
+// }
+
+//! todo : WAP to print odd numbers from 30-40 in reverse order using for loop
+// for(let i = 40; i >= 30; i--){
+//     if (i%2 != 0){
+//         console.log(i);
+//     }  
+// }
+
+// --------------------------------------------------
+// ! Collecting data from enduser and printing
+// let fname = prompt('Enter your first name')
+// let lname = prompt('Enter your last name')
+// console.log(`My name is ${fname} ${lname}`);
+
+// todo : WAP to add two numbers while collection values from enduser
+// let num1 = Number(prompt(`Enter number 1`))
+// let num2 = Number(prompt(`Enter number `))
+// let res = num1 + num2
+// console.log(`Sum of ${num1} + ${num2} is ${res}`);
+
+// todo : WAP to check even or odd while taking value for enduser
+
+// let input = prompt('Enter number')
+// let num = Number(input)
+// if (!Number.isNaN(num)){
+//     if (num %2 == 0){
+//         console.log(`${num} is even`);
+//     }
+//     else{
+//         console.log(`${num} is odd`);
+//     }
+// }
+// else{
+//     console.log(`${input} is NOT a NUMBER`);
+// }
+
+// todo : Print odd numbers from start-end while taking values from end-user
+
+// let num1 = Number(prompt(`Enter number 1`))
+// let num2 = Number(prompt(`Enter number 2`))
+// for (let i = num1; i <= num2; i++) {
+//     if (i%2 != 0){
+//         console.log(i);
+//     }
+// }
+
+// todo : WAP to print sum of number from start-end
+// let num1 = Number(prompt(`Enter number 1`))
+// let num2 = Number(prompt(`Enter number 2`))
+// let sum = 0
+// for(let i = num1; i <= num2; i++){
+//     sum += i 
+// }
+// console.log(sum);
+
+// todo : WAP to print the factorial of 5
+// console.warn('Factorial of N');
+
+// let n = Number(prompt('Enter a number to find factorial'))
+// let factorial = 1
+// for(let i = 5; i >= 1 ; i--){
+//     factorial = factorial * i
+// }
+// console.log(factorial);
+
+
+// todo : WAP to display multiplication table
+// let n = Number(prompt('Which table you want ? '))
+// for(let i = 1; i <= 10 ; i++){
+//     console.log(`${n} x ${i} = ${n*i}`);
+// }
+
+// todo : Interview Question
+// let i = 0
+// for(console.log("Hello") ; i <= 5 ; console.log("World")){
+//     console.log("Bye");
+//     i = i + 2
+// }
+
+
+// ! While Looping Statements
+// console.error("While loop");
+
+// todo : Print odd numbers from start-end while taking values from end-user
+
+// let i = 1;
+// let num = Number(prompt('Enter a number: '))
+// while(i <= num){
+//     if ( i%2 != 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// todo : print numbers from 5-10 in reverse order
+// let i = 10
+// while(i>=5){
+//     console.log(i);
+//     i--;
+// }
+
+// todo : print odd numbers from 55-65 in reverse order
+// let i = 65;
+// while(i>=55){
+//     if (i%2 == 1){
+//         console.log(i);
+//     }
+//     i--
+// }
+
+// todo : print even numbers from 55-65
+// let i = 55;
+// while(i <= 65){
+//     if (i%2 == 0)
+//         console.log(i);
+//     i++;
+// }
+
+
