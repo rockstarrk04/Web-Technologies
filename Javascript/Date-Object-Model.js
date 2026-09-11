0bf0235a52eaab134afb34cafe44a788f3175973
rockstarrk04 <jramkumar2004@gmail.com>
2026-09-11 21:48:16 +0530

    Intro to Date Object Model

--- Javascript/Date-Object-Model.js
+++ Javascript/Date-Object-Model.js
@@ -1,16 +1,13 @@
-//! Date Object Mode
+//! Date Object Model : It is used to display current data and time & it returns date in the form of string
+
+let str =  Date()
+console.log(str);
+console.log(str.slice(0,7));
+
+//todo : converting Date() to object using "new" keyword because to access the properties of object inbuilt method or else it  returns date in form of string
 
 
 let dateObj = new Date()
 console.log(dateObj);
 console.log(dateObj.getDate());
-console.log(dateObj.getMonth()+1);
-console.log(dateObj.getFullYear());
-
-console.log(dateObj.getHours());
-
-
-
-
-
-
+console.log(dateObj.getDay());
